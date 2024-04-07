63b4972474f2ccef5fe98abaecd312526ca36d83
ibtissammassa <bettysama16@gmail.com>
2024-04-07 02:02:37 +0000

    edits

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Footer() {
   return (
     <footer className="py-9">
-      <div className="container grid items-center gap-4 px-4 text-center md:px-6 lg:gap-10">
+      <div className="container flex justify-between gap-4 px-4 text-center md:px-6 lg:gap-10">
         <div className="space-y-2">
           <p className="text-sm text-gray-500 dark:text-gray-400">
             © 2024 SMSBlaster. All rights reserved.
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -11,7 +11,7 @@ function NavBar() {
               <span>Have a question? Call us!</span>
             </div>
             <Link
-              className="underline hover:text-gray-900 dark:hover:text-gray-50"
+              className="underline hover:text-gray-900"
               href="#">
               +1 (555) 123-4567
             </Link>
@@ -31,29 +31,29 @@ function NavBar() {
             </Link>
             <div className="hidden items-center space-x-3 md:flex">
               <Link
-                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:hover:text-gray-50"
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900"
                 href="#features">
                 Features
               </Link>
               <Link
-                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:hover:text-gray-50"
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900"
                 href="#how">
                 How it works?
               </Link>
               <Link
-                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:hover:text-gray-50"
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900"
                 href="#contact">
                 Contact
               </Link>
             </div>
             {/* <div className="ml-auto">
             <Link
-              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300 dark:border-gray-800"
+              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:focus-visible:ring-gray-300 dark:border-gray-800"
               href="#">
               Log in
             </Link>
             <Link
-              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-900 dark:hover:bg-gray-900/90 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300 dark:border-gray-800"
+              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-900 dark:hover:bg-gray-900/90 dark:focus-visible:ring-gray-300 dark:border-gray-800"
               href="#">
               Sign up
             </Link>
